748a626948f6ecaf926b42bcba1fb416dbb992d6
cibelesmora <cibelesmora@gmail.com>
2025-06-24 08:42:37 +0200

    Update storage.php

--- storage.php
+++ storage.php
@@ -12,7 +12,7 @@ use MicrosoftAzure\Storage\Common\Internal\Resources;
 
 // Configuración
 $connectionString = getenv("AZURE_STORAGE_CONNECTION_STRING");
-$containerName = "comprimidos"; // Cambia esto por el nombre de tu contenedor
+$containerName = "almacencefirecm"; // Cambia esto por el nombre de tu contenedor
 
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
